dc9bc6e8c46cec914a31914feaec1d5719fcaaa8
Vinny Ruia <vruia@me.com>
2020-03-15 18:35:00 -0400

    visualization is not hanging gdb, but now poses from path are empty

--- src/visualize.cpp
+++ src/visualize.cpp
@@ -212,80 +212,80 @@ void Visualize::publishNode3DCosts(Node3D* nodes, int width, int height, int dep
 //                                    COST HEATMAP 2D
 //###################################################
 void Visualize::publishNode2DCosts(Node2D* nodes, int width, int height) {
-  visualization_msgs::MarkerArray costCubes;
-  visualization_msgs::Marker costCube;
+  // visualization_msgs::MarkerArray costCubes;
+  // visualization_msgs::Marker costCube;
 
-  float min = 1000;
-  float max = 0;
-  bool once = true;
-  float red = 0;
-  float green = 0;
-  float blue = 0;
-  int count = 0;
+  // float min = 1000;
+  // float max = 0;
+  // bool once = true;
+  // float red = 0;
+  // float green = 0;
+  // float blue = 0;
+  // int count = 0;
 
-  ColorGradient heatMapGradient;
-  heatMapGradient.createDefaultHeatMapGradient();
+  // ColorGradient heatMapGradient;
+  // heatMapGradient.createDefaultHeatMapGradient();
 
-  float values[width * height];
+  // float values[width * height];
 
-  // ________________________________
-  // DETERMINE THE MAX AND MIN VALUES
-  for (int i = 0; i < width * height; ++i) {
-    values[i] = 1000;
+  // // ________________________________
+  // // DETERMINE THE MAX AND MIN VALUES
+  // for (int i = 0; i < width * height; ++i) {
+  //   values[i] = 1000;
 
-    // set the minimum for the cell
-    if (nodes[i].isDiscovered()) {
-      values[i] = nodes[i].getG();
+  //   // set the minimum for the cell
+  //   if (nodes[i].isDiscovered()) {
+  //     values[i] = nodes[i].getG();
 
-      // set a new minimum
-      if (values[i] > 0 && values[i] < min) { min = values[i]; }
+  //     // set a new minimum
+  //     if (values[i] > 0 && values[i] < min) { min = values[i]; }
 
-      // set a new maximum
-      if (values[i] > 0 && values[i] > max) { max = values[i]; }
-    }
-  }
+  //     // set a new maximum
+  //     if (values[i] > 0 && values[i] > max) { max = values[i]; }
+  // //  }
+  // }
 
-  // _______________
-  // PAINT THE CUBES
-  for (int i = 0; i < width * height; ++i) {
-    // if a value exists continue
-    if (nodes[i].isDiscovered()) {
-      count++;
+  // // _______________
+  // // PAINT THE CUBES
+  // for (int i = 0; i < width * height; i++) {
+  //   // if a value exists continue
+  //   if (nodes[i].isDiscovered()) {
+  //     count++;
 
-      // delete all previous markers
-      if (once) {
-        costCube.action = 3;
-        once = false;
-      } else {
-        costCube.action = 0;
-      }
+  //     // delete all previous markers
+  //     if (once) {
+  //       costCube.action = 3;
+  //       once = false;
+  //     } else {
+  //       costCube.action = 0;
+  //     }
 
 
-      costCube.header.frame_id = "path";
-      costCube.header.stamp = ros::Time::now();
-      costCube.id = i;
-      costCube.type = visualization_msgs::Marker::CUBE;
-      values[i] = (values[i] - min) / (max - min);
-      costCube.scale.x = Constants::cellSize;
-      costCube.scale.y = Constants::cellSize;
-      costCube.scale.z = 0.1;
-      costCube.color.a = 0.5;
-      heatMapGradient.getColorAtValue(values[i], red, green, blue);
-      costCube.color.r = red;
-      costCube.color.g = green;
-      costCube.color.b = blue;
-      // center in cell +0.5
-      costCube.pose.position.x = (i % width + 0.5) * Constants::cellSize;
-      costCube.pose.position.y = ((i / width) % height + 0.5) * Constants::cellSize;
-      costCubes.markers.push_back(costCube);
-    }
-  }
+  //     costCube.header.frame_id = "path";
+  //     costCube.header.stamp = ros::Time::now();
+  //     costCube.id = i;
+  //     costCube.type = visualization_msgs::Marker::CUBE;
+  //     values[i] = (values[i] - min) / (max - min);
+  //     costCube.scale.x = Constants::cellSize;
+  //     costCube.scale.y = Constants::cellSize;
+  //     costCube.scale.z = 0.1;
+  //     costCube.color.a = 0.5;
+  //     heatMapGradient.getColorAtValue(values[i], red, green, blue);
+  //     costCube.color.r = red;
+  //     costCube.color.g = green;
+  //     costCube.color.b = blue;
+  //     // center in cell +0.5
+  //     costCube.pose.position.x = (i % width + 0.5) * Constants::cellSize;
+  //     costCube.pose.position.y = ((i / width) % height + 0.5) * Constants::cellSize;
+  //     costCubes.markers.push_back(costCube);
+  //   }
+  // }
 
-  if (Constants::coutDEBUG) {
-    std::cout << "2D min cost: " << min << " | max cost: " << max << std::endl;
-    std::cout << count << " 2D nodes expanded " << std::endl;
-  }
+  // if (Constants::coutDEBUG) {
+  //   std::cout << "2D min cost: " << min << " | max cost: " << max << std::endl;
+  //   std::cout << count << " 2D nodes expanded " << std::endl;
+  // }
 
-  // PUBLISH THE COSTCUBES
-  pubNodes2DCosts.publish(costCubes);
+  // // PUBLISH THE COSTCUBES
+  // pubNodes2DCosts.publish(costCubes);
 }
